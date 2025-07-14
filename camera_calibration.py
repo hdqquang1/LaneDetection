7bb66eb8828fcbeb8be35028adfb9479d2b872b9
hdqquang1 <dhuy9648@uni.sydney.edu.au>
2025-07-14 16:26:19 +1000

    Update camera calibration

--- camera_calibration.py
+++ camera_calibration.py
@@ -1,10 +1,11 @@
-import glob
-import pickle
+import os
 import cv2 as cv
 import numpy as np
+import glob
+import pickle
 
-# Inside corners in chessboard
-NX = 9
+# Numbers of corners in chessboard
+NX = 7
 NY = 5
 
 # Termination criteria
@@ -18,30 +19,86 @@ objp[:, :2] = np.mgrid[0:NX, 0:NY].T.reshape(-1, 2)
 objpoints = []  # 3D points in real world space
 imgpoints = []  # 2D poitns in image plane
 
-# Read all images from path
-images = glob.glob('camera_calibration/*.jpg')
+# Initialize camera
+cap = cv.VideoCapture(0)
 
-for fname in images:
+# Frames for calibration
+folderName = 'frame_calib'
+if not os.path.exists(folderName):
+    os.makedirs(folderName)
+    print(f'Created folder {'frame_calib'}.')
+imgCount = 0
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if ret:
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        # Find the chess board corners
+        retChessboard, corners = cv.findChessboardCorners(gray, (NX, NY), None)
+
+        # If found, add object points and image points
+        if retChessboard:
+            objpoints.append(objp)
+
+            corners2 = cv.cornerSubPix(
+                gray, corners, (max(NX, NY)+2, max(NX, NY)+2), (-1, -1), criteria)
+            imgpoints.append(corners2)
+
+            frameChessboard = frame.copy()
+            cv.drawChessboardCorners(frameChessboard, (NX, NY),
+                                     corners2, retChessboard)
+            cv.imshow('frameChessboard', frameChessboard)
+
+        cv.imshow('frame', frame)
+
+        key = cv.waitKey(1) & 0xFF
+
+        if key == ord('q'):
+            filename = f'{imgCount:02}.jpg'
+            filePath = os.path.join(folderName, filename)
+            cv.imwrite(filePath, frame)
+            print(f'Saved {filePath}.')
+            imgCount += 1
+            continue
+        elif key == 27:
+            break
+
+cap.release()
+cv.destroyAllWindows()
+
+imgsNames = '*.jpg'
+imgs = glob.glob(os.path.join(folderName, imgsNames))
+
+for fname in imgs:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Find the chess board corners
     ret, corners = cv.findChessboardCorners(gray, (NX, NY), None)
 
-    # If found, add object points and image points
-    if ret:
+    # If found, add object points, image points (after refining them)
+    if ret == True:
         objpoints.append(objp)
 
-        corners2 = cv.cornerSubPix(gray, corners, (max(NX, NY)+2, max(NX, NY)+2),
-                                   (-1, -1), criteria)
-        imgpoints.append(corners)
+        winSize = max(NX, NY) + 4
+        corners2 = cv.cornerSubPix(
+            gray, corners, (winSize, winSize), (-1, -1), criteria)
+        imgpoints.append(corners2)
 
-# Calibrate camera
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints,
-                                                  gray.shape[::-1], None, None)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (NX, NY), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
 
-# Save camera calibration result
-calibration = {}
-calibration['mtx'] = mtx
-calibration['dist'] = dist
-pickle.dump(calibration, open('calibration.p', 'wb'))
+cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(
+    objpoints, imgpoints, gray.shape[::-1], None, None)
+
+# Save in pickle
+calib = {}
+calib['mtx'] = mtx
+calib['dist'] = dist
+pickle.dump(calib, open('calibration.p', 'wb'))
--- camera_calibration_test.py
+++ camera_calibration_test.py
@@ -5,17 +5,27 @@ calibration = pickle.load(open('calibration.p', 'rb'))
 mtx = calibration["mtx"]
 dist = calibration["dist"]
 
-img = cv.imread('camera_calibration/calibration3.jpg')
-h,  w = img.shape[:2]
-newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+cap = cv.VideoCapture(0)
 
-# undistort
-dst = cv.undistort(img, mtx, dist, None, newcameramtx)
- 
-# crop the image
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
-cv.imshow('dst', dst)
-cv.waitKey(0)
+while cap.isOpened():
+    ret, frame = cap.read()
 
+    if ret:
+        h,  w = frame.shape[:2]
+        newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+        # undistort
+        dst = cv.undistort(frame, mtx, dist, None, newcameramtx)
+        
+        # crop the image
+        x, y, w, h = roi
+        dst = dst[y:y+h, x:x+w]
+        cv.imshow('dst', dst)
+
+        cv.imshow('frame', frame)
+
+        if cv.waitKey(1) & 0xFF == 27:
+            break
+
+cap.release()
 cv.destroyAllWindows()
